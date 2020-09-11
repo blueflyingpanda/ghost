84c8b87bb3ce8480cbfbc0a3ff8aded20557aced
Ilya <m.s.v.inkognito@gmail.com>
2020-09-12 00:23:52 +0300

    basic movement

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ function on_mouse_move(event){
 	if (window.innerWidth/2 > event.x)
 		document.getElementById("face").style.left = "10px";
 	else
-		document.getElementById("face").style.left = "50px";
-}
+		document.getElementById("face").style.left = "40px";
+	var follow = document.getElementById("ghost");
+	follow.style.top = event.clientY - 50 + "px";
+	follow.style.left = event.clientX - 50 + "px";
+};
 
 window.addEventListener('mousemove', on_mouse_move);
 
