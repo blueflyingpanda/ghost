f6615029614d93eeeb21d38eef594cf0c5125955
Ilya <m.s.v.inkognito@gmail.com>
2020-09-12 00:25:21 +0300

    no movement

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function on_mouse_move(event){
 		document.getElementById("face").style.left = "10px";
 	else
 		document.getElementById("face").style.left = "40px";
-	var follow = document.getElementById("ghost");
-	follow.style.top = event.clientY - 50 + "px";
-	follow.style.left = event.clientX - 50 + "px";
+	//var follow = document.getElementById("ghost");
+	//follow.style.top = event.clientY - 50 + "px";
+	//follow.style.left = event.clientX - 50 + "px";
 };
 
 window.addEventListener('mousemove', on_mouse_move);
