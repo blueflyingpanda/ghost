6b5dfa9bfe96496b9106f61f46c24355ca5e37e8
Ilya <m.s.v.inkognito@gmail.com>
2020-09-12 11:30:24 +0300

    jq

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
+var mouseX, mouseY, follow, rect;
+
 function on_mouse_move(event){
-	console.log(`x = ${event.x}		y = ${event.y}`);
 	if (window.innerWidth/2 > event.x)
 		document.getElementById("face").style.left = "10px";
 	else
 		document.getElementById("face").style.left = "40px";
-	var follow = document.getElementById("ghost");
-	follow.style.top = event.clientY - 50 + "px";
-	follow.style.left = event.clientX - 50 + "px";
+	mouseX = event.x;
+	mouseY = event.y;
 };
 
 window.addEventListener('mousemove', on_mouse_move);
-
-
