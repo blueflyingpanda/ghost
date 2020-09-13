f72e6107429ba13c567e073d05a88cda5e2cc019
Ilya <m.s.v.inkognito@gmail.com>
2020-09-13 23:49:55 +0300

    no jq

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-var mouseX, mouseY, follow, rect;
+let face, follow, rect, left, topp, mouseX, mouseY, step, dist;
 
-function on_mouse_move(event){
-	if (window.innerWidth/2 > event.x)
+const MID_X = 85;
+const MID_Y = 75;
+const REFRESH = 10;
+const MAX_STEP = 20;
+
+function onMouseMove(event){
+	if (left > mouseX + MID_X)
 		document.getElementById("face").style.left = "10px";
-	else
+	else if (left < mouseX - MID_X)
 		document.getElementById("face").style.left = "40px";
+	else
+		document.getElementById("face").style.left = face;
 	mouseX = event.x;
 	mouseY = event.y;
-};
+}
 
-window.addEventListener('mousemove', on_mouse_move);
+function followMouse(){
+	if (mouseX == left && mouseY == topp)
+		return ;
+	dist = Math.sqrt((mouseX - left)*(mouseX - left) + (mouseY - topp)*(mouseY - topp));
+	step = (dist/10 > MAX_STEP) ? MAX_STEP : dist/10; 
+	left= step/dist * (mouseX - left) + left;
+	topp = step/dist * (mouseY - topp) + topp;
+	follow.style.top = topp - MID_Y + "px";
+	follow.style.left = left - MID_X + "px";
+}
+
+face = document.getElementById("face").style.left;
+follow = document.getElementById("ghost");
+rect = follow.getBoundingClientRect();
+left = rect.x;
+topp = rect.y;
+follow.style.top = topp - MID_Y + "px";
+follow.style.left = left - MID_X + "px";
+mouseX = rect.x;
+mouseY = rect.y;
+window.setInterval(followMouse, REFRESH);
+window.addEventListener('mousemove', onMouseMove);
